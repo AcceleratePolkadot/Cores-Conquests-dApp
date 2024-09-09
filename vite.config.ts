91acbf0db2ef11f2c83ab57809721a19db7f9eee
Aaron Bassett <aaronbassett@gmail.com>
2024-09-09 19:09:35 +0100

    refactor(vite): add server port and ./src directory alias to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import react from "@vitejs/plugin-react-swc";
 /// <reference types="vitest" />
+
+import * as path from "node:path";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config https://vitest.dev/config
+// https://vitejs.dev/config
 export default defineConfig({
+  server: {
+    port: 5001,
+  },
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "happy-dom",
