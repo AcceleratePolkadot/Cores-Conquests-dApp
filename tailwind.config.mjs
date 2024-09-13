c13c5d00127c93520a7dacc58b2951e4f7d61320
Aaron Bassett <aaronbassett@gmail.com>
2024-09-13 16:25:31 -0400

    fix: move custom trailwind theme colors into extend

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,18 +5,20 @@ import flowbite from "flowbite-react/tailwind";
 export default {
   content: ["./src/**/*.{mjs,js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    colors: {
-      primary: {
-        50: "#eff6ff",
-        100: "#dbeafe",
-        200: "#bfdbfe",
-        300: "#93c5fd",
-        400: "#60a5fa",
-        500: "#3b82f6",
-        600: "#2563eb",
-        700: "#1d4ed8",
-        800: "#1e40af",
-        900: "#1e3a8a",
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+        },
       },
     },
     fontFamily: {
@@ -65,7 +67,6 @@ export default {
         "monospace",
       ],
     },
-    extend: {},
   },
   plugins: [flowbite.plugin()],
 };
