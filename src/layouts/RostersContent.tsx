5f9cd55bc786d42a83b62e96c3127b607fe19e82
Aaron Bassett <aaronbassett@gmail.com>
2024-09-15 13:57:10 -0400

    feat: add Roster details card to layout

--- src/layouts/RostersContent.tsx
+++ src/layouts/RostersContent.tsx
@@ -3,12 +3,14 @@ import { useRosters } from "@/contexts/Rosters";
 import { Card } from "flowbite-react";
 import type React from "react";
 
+import RosterDetail from "@/components/Rosters/RosterDetail";
+
 const RostersContent: React.FC = () => {
   const { activeRoster } = useRosters();
 
   return (
     <>
-      <div className="grid grid-cols-4 gap-6 p-6 dark:bg-gray-900">
+      <div className="grid grid-cols-1 gap-2 p-2 xl:grid-cols-5 xl:gap-4 xl:p-4 2xl:grid-cols-4 2xl:gap-6 2xl:p-6 dark:bg-gray-900">
         {!activeRoster ? (
           <>
             <div className="col-span-full border-gray-200 border-b pb-2 dark:border-gray-700">
@@ -20,19 +22,15 @@ const RostersContent: React.FC = () => {
           </>
         ) : (
           <>
-            <div className="col-span-full">
+            <div className="col-span-full border-gray-200 border-b pb-2 dark:border-gray-700">
               <h1 className="font-semibold text-2xl text-gray-900 dark:text-white">
                 {activeRoster.title.asText()}
               </h1>
             </div>
 
-            <div className="col-span-1">
-              <div className="space-y-6">
-                <Card>
-                  <h3 className="mb-1 font-bold text-2xl text-gray-900 dark:text-white">
-                    Roster Details
-                  </h3>
-                </Card>
+            <div className="col-span-full xl:col-span-2 2xl:col-span-1">
+              <div className="space-y-2 xl:space-y-4 2xl:space-y-6">
+                <RosterDetail />
                 <Card>
                   <h3 className="mb-1 font-bold text-2xl text-gray-900 dark:text-white">
                     Nominations
@@ -41,7 +39,7 @@ const RostersContent: React.FC = () => {
               </div>
             </div>
 
-            <div className="col-span-3">
+            <div className="col-span-full xl:col-span-3 2xl:col-span-3">
               <Card>
                 <h3 className="mb-1 font-bold text-2xl text-gray-900 dark:text-white">Members</h3>
               </Card>
