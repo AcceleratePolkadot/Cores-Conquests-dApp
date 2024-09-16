3b9a0dc4497137d1822b884fbb1c5da0fe962ab9
Aaron Bassett <aaronbassett@gmail.com>
2024-09-16 17:58:26 -0400

    fix: remove !important override and fix CSS styles of active link in table pagination

--- src/components/Table/TablePagination.tsx
+++ src/components/Table/TablePagination.tsx
@@ -107,7 +107,7 @@ const TablePagination = <T,>({ paginateItems, setCurrentItems }: TablePagination
           pageLinkClassName="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           previousLinkClassName="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           nextLinkClassName="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          activeLinkClassName="font-bold text-primary-900 !bg-gray-100"
+          activeLinkClassName="font-semibold text-primary-600 dark:text-primary-400"
         />
       </nav>
     </>
