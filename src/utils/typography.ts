95cdf4701a1b05bedddf03e6f3fcedb04c607844
Aaron Bassett <aaronbassett@gmail.com>
2024-11-22 14:55:53 +0000

    ✨ feat(utils): add string case conversion utils

---
+++ src/utils/typography.ts
@@ -0,0 +1,35 @@
+export const toHeaderCase = (str: string): string => {
+  return String(str)
+    .replace(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g, "")
+    .replace(/([a-z])([A-Z])/g, (_, a, b) => `${a}_${b.toLowerCase()}`)
+    .replace(/[^A-Za-z0-9]+|_+/g, " ")
+    .toLowerCase()
+    .replace(/( ?)(\w+)( ?)/g, (_, a, b, c) => a + b.charAt(0).toUpperCase() + b.slice(1) + c);
+};
+
+export const toSentenceCase = (str: string): string => {
+  const textcase = String(str)
+    .replace(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g, "")
+    .replace(/([a-z])([A-Z])/g, (_, a, b) => `${a}_${b.toLowerCase()}`)
+    .replace(/[^A-Za-z0-9]+|_+/g, " ")
+    .toLowerCase();
+
+  return textcase.charAt(0).toUpperCase() + textcase.slice(1);
+};
+
+export const toApTitleCase = (str: string): string => {
+  const stopwords = "a an and at but by for in nor of on or so the to up yet".split(" ");
+  const splitOn = /(\s+|[-‑–—,:;!?()])/;
+
+  return str
+    .split(splitOn)
+    .map((word, index, all) => {
+      if (index % 2) return word;
+      const lower = word.toLowerCase();
+      if (index !== 0 && index !== all.length - 1 && stopwords.includes(lower)) {
+        return lower;
+      }
+      return lower.charAt(0).toUpperCase() + lower.slice(1);
+    })
+    .join("");
+};
