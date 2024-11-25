fa8e5befb1dd07e5ef41c8c3bba01104d08cb7f2
Aaron Bassett <aaronbassett@gmail.com>
2024-11-25 13:30:19 +0000

    ✨ feat: add formatter for token balances

---
+++ src/hooks/useBalanceFormatter.tsx
@@ -0,0 +1,24 @@
+import { formatBalance } from "@polkadot/util";
+import { useChainSpecData } from "@reactive-dot/react";
+import type { BigNumber } from "bignumber.js";
+import { useCallback } from "react";
+
+const useBalanceFormatter = (withSi = true, forceUnit = undefined) => {
+  const chainSpecData = useChainSpecData();
+
+  const balanceFormatter = useCallback(
+    (balance: BigNumber) => {
+      return formatBalance(balance.toString(), {
+        decimals: chainSpecData.properties.tokenDecimals,
+        withSi: withSi,
+        forceUnit: forceUnit,
+        withZero: false,
+      });
+    },
+    [chainSpecData, withSi, forceUnit],
+  );
+
+  return { balanceFormatter };
+};
+
+export default useBalanceFormatter;
