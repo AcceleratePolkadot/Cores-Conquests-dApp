704651e6c341fe885e54f76d98a9f1fafe449d3f
Aaron Bassett <aaronbassett@gmail.com>
2024-11-25 13:32:38 +0000

    ♻️ refactor: export Address type for Accounts

--- src/contexts/ActiveAccount/types.ts
+++ src/contexts/ActiveAccount/types.ts
@@ -1,5 +1,7 @@
 import type { WalletAccount } from "@reactive-dot/core/wallets.js";
 
+export type Address = WalletAccount["address"];
+
 export interface ActiveAccountContextInterface {
   activeAccount: WalletAccount | undefined;
   setActiveAccount: (account: WalletAccount | undefined) => void;
